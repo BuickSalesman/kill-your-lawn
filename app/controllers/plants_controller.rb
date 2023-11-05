4bcb04fa7efdc07b9ff6701cfe23c5ec54d57df4
BuickSalesman <adam.odonnell.e@gmail.com>
2023-11-05 16:49:31 -0500

    added search feture

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,8 @@
 class PlantsController < ApplicationController
   def index
-    response = HTTP.get("https://perenual.com/api/species-list?key=#{ENV["PERENUAL_API_KEY"]}")
+    response = HTTP.get("https://perenual.com/api/species-list?key=#{ENV["PERENUAL_API_KEY"]}&q=#{CGI.escape params[:query]}")
+    pp response
+    pp response.parse
     data = response.parse["data"].shuffle
     render json: data
   end
